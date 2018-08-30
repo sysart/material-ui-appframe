8bac92719de5f72bda5674fb1058a15cb51e3a1b
Mikko Kumara <mikko.kumara@gmail.com>
2018-08-30 10:46:19 +0300

    Add className prop to MainContent

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -1,43 +1,52 @@
 import * as React from "react"
 
+import { withStyles } from "@material-ui/core"
+import { Theme } from "@material-ui/core/styles"
+import { CSSProperties, WithStyles } from "@material-ui/core/styles/withStyles"
+import classNames from "classnames"
 import { WithLegacyCSSLayout } from "../layout/LegacyCSSLayout"
-import { styled } from "../utilities/styled"
 import { WithWidth } from "../utilities/WithWidth"
 
 interface Props {
 	children: JSX.Element | JSX.Element[]
+	className?: string
 }
 
-const StyledMain = styled("main")((theme) => ({
-	gridArea: "content",
-	backgroundColor: theme.palette.background.default,
-	padding: theme.spacing.unit * 3,
-	overflowY: "auto"
-}))
+const styles = (theme: Theme) => ({
+	main: {
+		gridArea: "content",
+		backgroundColor: theme.palette.background.default,
+		padding: theme.spacing.unit * 3,
+		overflowY: "auto"
+	} as CSSProperties
+})
 
 /**
  * The main content of your application.
  */
-export const MainContent = (props: Props) => (
-	<WithLegacyCSSLayout>
-		{({ legacyMobileCssEnabled, appBarHeight, bottomNavigationHeight }) => (
-			<WithWidth>
-				{({ width }) => (
-					<StyledMain
-						style={
-							(legacyMobileCssEnabled &&
-								(width === "xs" || width === "sm") && {
-									overflowY: "visible",
-									marginTop: appBarHeight,
-									marginBottom: bottomNavigationHeight
-								}) ||
-							undefined
-						}
-					>
-						{props.children}
-					</StyledMain>
-				)}
-			</WithWidth>
-		)}
-	</WithLegacyCSSLayout>
+export const MainContent = withStyles(styles)(
+	(props: Props & WithStyles<"main">) => (
+		<WithLegacyCSSLayout>
+			{({ legacyMobileCssEnabled, appBarHeight, bottomNavigationHeight }) => (
+				<WithWidth>
+					{({ width }) => (
+						<main
+							className={classNames(props.classes.main, props.className)}
+							style={
+								(legacyMobileCssEnabled &&
+									(width === "xs" || width === "sm") && {
+										overflowY: "visible",
+										marginTop: appBarHeight,
+										marginBottom: bottomNavigationHeight
+									}) ||
+								undefined
+							}
+						>
+							{props.children}
+						</main>
+					)}
+				</WithWidth>
+			)}
+		</WithLegacyCSSLayout>
+	)
 )
